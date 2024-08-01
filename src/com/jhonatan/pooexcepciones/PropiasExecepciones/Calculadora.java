fe8e907046351622183624e291fe9009c57692e2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:46:04 -0500

    class Calculadora: agregando la exepcion creada

--- src/com/jhonatan/pooexcepciones/PropiasExecepciones/Calculadora.java
+++ src/com/jhonatan/pooexcepciones/PropiasExecepciones/Calculadora.java
@@ -1,7 +1,11 @@
 package com.jhonatan.pooexcepciones.PropiasExecepciones;
 
 public class Calculadora {
-    public double dividir(int numerador, int dividendo) throws ArithmeticException{
+    public double dividir(int numerador, int dividendo) throws DivisionPorZeroException {
+        if (dividendo == 0) {
+            // lanzamos la exception
+            throw new DivisionPorZeroException("No se puede devidir por cero");
+        }
         return numerador / (double) dividendo;
     }
 }
