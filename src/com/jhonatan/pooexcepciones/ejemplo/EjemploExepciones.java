711ab263ff9995bade2d2b899bb54c14aa50e585
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 14:44:08 -0500

    class EjemploExepciones: ejemplo de una excepcion

--- src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
+++ src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
@@ -2,7 +2,13 @@ package com.jhonatan.pooexcepciones.ejemplo;
 
 public class EjemploExepciones {
     public static void main(String[] args) {
-        
+        try {
+            int division = 10 / 0;
+            System.out.println(division);
+        } catch (Exception e) {
+            System.out.println(" Capturamos la excepcion en tiempo de ejecucion: "+e.getMessage());
+        }
+        System.out.println("Fin Programa");
     }
-    
+
 }
