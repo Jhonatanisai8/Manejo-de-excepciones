1507da1b67bb1c44e87cdf12f64e14428ad4ca88
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:23:59 -0500

    class EjemploExepciones : pidiendo el divisor al usuario

--- src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
+++ src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
@@ -1,17 +1,22 @@
 package com.jhonatan.pooexcepciones.ejemplo;
 
+import javax.swing.JOptionPane;
+
 public class EjemploExepciones {
     public static void main(String[] args) {
+        
+        String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
+        int dividor = Integer.parseInt(valor);
+
         try {
-            int division = 10 / 5;
-            System.out.println(division);
+            int division = 10 / dividor;
+            System.out.println("Resultado: "+division);
         } catch (ArithmeticException e) {
-            System.out.println(" Capturamos la excepcion en tiempo de ejecucion: "+e.getMessage());
-        }finally{
+            System.out.println(" Capturamos la excepcion en tiempo de ejecucion: " + e.getMessage());
+        } finally {
             System.out.println("Ejecuta siempre con o sin excepcion");
         }
         System.out.println("Fin Programa");
     }
 
 }
-         
