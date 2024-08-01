731942646eaccbbf4ca6bac06d63b41d236345c0
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:43:26 -0500

    class DivisionPorZeroException: clase hija de Exception

---
+++ src/com/jhonatan/pooexcepciones/PropiasExecepciones/DivisionPorZeroException.java
@@ -0,0 +1,7 @@
+package com.jhonatan.pooexcepciones.PropiasExecepciones;
+
+public class DivisionPorZeroException extends Exception {
+    public DivisionPorZeroException(String mensaje) {
+        super(mensaje);
+    }
+}
