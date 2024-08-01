5152c95a35b0e1c1f2a1f7e376232b15d78fdf67
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:36:21 -0500

    class Calculadora : por el momento solo tiene un metodo que es dividir

---
+++ src/com/jhonatan/pooexcepciones/PropiasExecepciones/Calculadora.java
@@ -0,0 +1,7 @@
+package com.jhonatan.pooexcepciones.PropiasExecepciones;
+
+public class Calculadora {
+    public double dividir(int numerador, int dividendo) {
+        return numerador / (double) dividendo;
+    }
+}
