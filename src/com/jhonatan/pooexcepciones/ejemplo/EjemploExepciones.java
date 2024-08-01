ad8ffd26ac97d9e9c41aeb919c5b08fb1f2c14e4
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:32:00 -0500

    class EjemploExepciones : agregando el método main dentro de catch

--- src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
+++ src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
@@ -5,15 +5,18 @@ import javax.swing.JOptionPane;
 public class EjemploExepciones {
     public static void main(String[] args) {
 
+        String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
+        int dividor;
         try {
-            String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
-            int dividor = Integer.parseInt(valor);
+            dividor = Integer.parseInt(valor);
             int division = 10 / dividor;
             System.out.println("Resultado: " + division);
         } catch (NumberFormatException en) {
             System.out.println("Se detecto una excepcion, ingrese un valor numerico: " + en.getMessage());
+            main(args);
         } catch (ArithmeticException e) {
             System.out.println(" Capturamos la excepcion en tiempo de ejecucion: " + e.getMessage());
+            main(args);
         } finally {
             System.out.println("Ejecuta siempre con o sin excepcion");
         }
