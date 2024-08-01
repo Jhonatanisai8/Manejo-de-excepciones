e47f3eac7dbd9b58112aeb68d70b1e3355053448
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 15:26:08 -0500

    class EjemploExepciones : agregando mas tipo de excepciones

--- src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
+++ src/com/jhonatan/pooexcepciones/ejemplo/EjemploExepciones.java
@@ -4,13 +4,14 @@ import javax.swing.JOptionPane;
 
 public class EjemploExepciones {
     public static void main(String[] args) {
-        
-        String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
-        int dividor = Integer.parseInt(valor);
 
         try {
+            String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
+            int dividor = Integer.parseInt(valor);
             int division = 10 / dividor;
-            System.out.println("Resultado: "+division);
+            System.out.println("Resultado: " + division);
+        } catch (NumberFormatException en) {
+            System.out.println("Se detecto una excepcion, ingrese un valor numerico: " + en.getMessage());
         } catch (ArithmeticException e) {
             System.out.println(" Capturamos la excepcion en tiempo de ejecucion: " + e.getMessage());
         } finally {
