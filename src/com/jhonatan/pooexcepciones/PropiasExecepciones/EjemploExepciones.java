5e7ae88746feb65e36b835a5f76f1a0778eac366
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-02 10:18:14 -0500

    class EjemploExepciones: ejemplo con la nueva exception creada

--- src/com/jhonatan/pooexcepciones/PropiasExecepciones/EjemploExepciones.java
+++ src/com/jhonatan/pooexcepciones/PropiasExecepciones/EjemploExepciones.java
@@ -4,8 +4,37 @@ import javax.swing.JOptionPane;
 
 public class EjemploExepciones {
     public static void main(String[] args) {
+        System.out.println("=====MANEJO DE EXCEPCIONES=====");
+        ejemplo02();
+    }
 
-        /*instancia de calculadora */
+    public static void ejemplo02() {
+        /* instancia de calculadora */
+        Calculadora calculadora = new Calculadora();
+        String numerador = JOptionPane.showInputDialog("Ingrese un entero numerador: ");
+        String denominador = JOptionPane.showInputDialog("Ingrese un entero denominador: ");
+        try {
+            /* llamamos al metodo de dividir */
+            double division02 = calculadora.dividir(numerador, denominador);
+
+            System.out.println("Resultado de la division 02: " + division02);
+            // primera excepcion
+        } catch (NumberFormatException en) {
+            System.out.println("Se detecto una excepcion, ingrese un valor numerico: " + en.getMessage());
+            // segunda excepcion
+        } catch (DivisionPorZeroException e) {
+            System.out.println(" Capturamos la excepcion en tiempo de ejecucion: " + e.getMessage());
+        } catch (FormatoNumeroException ecp) {
+            System.out.println("Se detecto una excepcion: ingrese un numero valido: " + ecp.getMessage());
+            ecp.printStackTrace(System.out);
+        } finally { /* bloque que si hay o no excepcion se ejecuta */
+            System.out.println("Ejecuta siempre con o sin excepcion");
+        }
+        System.out.println("Fin Programa");
+    }
+
+    public static void ejemplo01() {
+        /* instancia de calculadora */
         Calculadora calculadora = new Calculadora();
         String valor = JOptionPane.showInputDialog("Ingrese un entero: ");
         int divisor;
@@ -15,16 +44,14 @@ public class EjemploExepciones {
             divisor = Integer.parseInt(valor);
             division = calculadora.dividir(10, divisor);
             System.out.println("Resultado: " + division);
- 
+
             // primera excepcion
         } catch (NumberFormatException en) {
             System.out.println("Se detecto una excepcion, ingrese un valor numerico: " + en.getMessage());
-            main(args);
             // segunda excepcion
         } catch (DivisionPorZeroException e) {
             System.out.println(" Capturamos la excepcion en tiempo de ejecucion: " + e.getMessage());
-            main(args);
-        } finally { /*bloque que si hay o no excepcion se ejecuta */
+        } finally { /* bloque que si hay o no excepcion se ejecuta */
             System.out.println("Ejecuta siempre con o sin excepcion");
         }
         System.out.println("Fin Programa");
